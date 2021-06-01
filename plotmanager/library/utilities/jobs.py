30b6e2e4586590c0007c58fd7e7859a0e56d3718
Gilbert Gong <gilbert.gong@gmail.com>
2021-05-31 17:07:02 -0700

    Add logging for uniq_temp_dir behavior

--- plotmanager/library/utilities/jobs.py
+++ plotmanager/library/utilities/jobs.py
@@ -24,8 +24,11 @@ def get_uniq_temp_dir(job, running_work):
         if work.temporary_directory in available_temp_directory:
             available_temp_directory.remove(work.temporary_directory)
     if len(available_temp_directory):
-        return available_temp_directory[0]
+        available = available_temp_directory[0]
+        logging.info('Found available unique temp dir: ', available)
+        return available
     else:
+        logging.info('No unique temp dir available')
         return None
 
 
@@ -46,6 +49,7 @@ def get_target_directories(job, running_work, drives_free_space, uniq_temp_dir=T
         destination_directory = job.destination_directory[job_offset % len(job.destination_directory)]
     if isinstance(job.temporary_directory, list):
         if uniq_temp_dir:
+            logging.info('Using uniq_temp_dir logic:')
             temporary_directory = get_uniq_temp_dir(job, running_work)
             if temporary_directory is None:
                 return None, None, None, job
